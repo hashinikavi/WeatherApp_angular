38e16e85d9a8fe054b918e1ae050c0b8f1acdcbc
hashinikavi <hashikavi1@gmail.com.com>
2023-11-12 22:41:14 +0530

    third commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,1 @@
-export const environment = {
-    production: false,
-    weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather',
-    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
-    XRapidAPIHostHeaderValue: 'open-weather-map27.p.rapidapi.com',
-    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: 'ac764a7204mshc35c56893ddd17ep193abbjsnf9f5eeeba795'
-
-};
+export const environment = { };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'ac764a7204mshc35c56893ddd17ep193abbjsnf9f5eeeba795'
+
+};
